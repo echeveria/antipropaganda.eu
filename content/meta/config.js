bdfe24e5524e2f395cc679b98d83e79bb0f60a00
stoyan.germanov <stoyan.germanov@dnhsoft.com>
2019-04-04 16:49:57 +0300

    add structure

--- content/meta/config.js
+++ content/meta/config.js
@@ -1,23 +1,23 @@
 module.exports = {
-  siteTitle: "HeroBlog - a GatsbyJS starter", // <title>
-  shortSiteTitle: "HeroBlog - GatsbyJS starter", // <title> ending for posts and pages
-  siteDescription: "HeroBlog is a GatsbyJS starter.",
-  siteUrl: "https://gatsby-starter-hero-blog.greglobinski.com",
+  siteTitle: "Антипропаганда - Ако винаги следваш, никога няма да водиш!", // <title>
+  shortSiteTitle: "Антипропаганда", // <title> ending for posts and pages
+  siteDescription: "Ако винаги следваш, никога няма да водиш!",
+  siteUrl: "https://antipropaganda.eu/",
   // pathPrefix: "",
-  siteImage: "preview.jpg",
-  siteLanguage: "en",
+  siteImage: "logo.png",
+  siteLanguage: "bg",
 
   /* author */
-  authorName: "greg lobinski",
-  authorTwitterAccount: "greglobinski",
+  authorName: "kihano",
+  authorTwitterAccount: "",
 
   /* info */
-  headerTitle: "greg lobinski",
-  headerSubTitle: "presents another one GatsbyJS starter",
+  headerTitle: "Антипропаганда",
+  headerSubTitle: "Ако винаги следваш, никога няма да водиш!",
 
   /* manifest.json */
-  manifestName: "HeroBlog - a GatsbyJS starter",
-  manifestShortName: "HeroBlog", // max 12 characters
+  manifestName: "Антипропаганда",
+  manifestShortName: "Антипропаганда", // max 12 characters
   manifestStartUrl: "/index.html",
   manifestBackgroundColor: "white",
   manifestThemeColor: "#666",
@@ -32,8 +32,8 @@ module.exports = {
 
   // social
   authorSocialLinks: [
-    { name: "github", url: "https://github.com/greglobinski" },
-    { name: "twitter", url: "https://twitter.com/greglobinski" },
-    { name: "facebook", url: "http://facebook.com/greglobinski" }
+    // { name: "github", url: "https://github.com/greglobinski" },
+    // { name: "twitter", url: "https://twitter.com/greglobinski" },
+    // { name: "facebook", url: "http://facebook.com/greglobinski" }
   ]
 };
